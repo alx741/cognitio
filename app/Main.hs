708acf2714c5b82514cd74d94067d9815e48628b
Daniel Campoverde <alx@sillybytes.net>
2017-09-14 21:01:20 -0500

    Basic plot functions

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import Lib
 
 main :: IO ()
-main = undefined
+main = do
+    plotVisualize [(1,2), (1.5, 3), (2,4)] [(1,3), (1.7, 2), (1.5, 3.5), (1,4)]
+    -- plotPNG "hola.png" [(1,2), (1.5, 3), (2,4)] [(1,3), (1.7, 2), (1.5, 3.5), (1,4)]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,19 @@
-module Lib
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Graphics.EasyPlot
+
+plotVisualize :: (Fractional a, Enum a, Show a) => [(a, a)] -> [(a, a)] -> IO ()
+plotVisualize d1 d2 = do
+    _  <- plot X11 $
+        [ Data2D [Title "Clase 1", Color Red] [] d1
+        , Data2D [Title "Clase 2", Color Blue] [] d2
+        ]
+    return ()
+
+plotPNG  :: (Fractional a, Enum a, Show a) => FilePath -> [(a, a)] -> [(a, a)] -> IO ()
+plotPNG fp d1 d2 = do
+    _  <- plot (PNG fp) $
+        [ Data2D [Title "Clase 1", Color Red] [] d1
+        , Data2D [Title "Clase 2", Color Blue] [] d2
+        ]
+    return ()
